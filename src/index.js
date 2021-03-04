0eb76d62cdcb17dbb094dffbde7367c116e0a2e9
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-03 22:23:12 -0500

    added the audio functionality

--- src/index.js
+++ src/index.js
@@ -3,20 +3,74 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
-const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+// const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
+const sounds = [
+  {
+    key: "Q",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    key: "W",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    key: "E",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+  {
+    key: "A",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  {
+    key: "S",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    key: "D",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+  {
+    key: "Z",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    key: "X",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  {
+    key: "C",
+    mp3: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
 
 const App = () => (
   <div id="display" className="display">
-    {keys.map((key, idx) => (
-      <Box text={key} key={idx} />
+    {sounds.map((sound, idx) => (
+      <Box text={sound.key} key={idx} audio={sound.mp3} />
     ))}
   </div>
 );
 
 class Box extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.audio = React.createRef();
+  }
+
+  playSound = () => {
+    this.audio.current.play();
+  };
   render() {
-    return <div className="box">{this.props.text}</div>;
+    const { text, audio } = this.props;
+
+    return (
+      <div className="box" onClick={this.playSound}>
+        {text}
+        <audio ref={this.audio} src={audio} className="clip" id={text} />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App keys={keys} />, document.querySelector("#drum-machine"));
+ReactDOM.render(<App />, document.querySelector("#drum-machine"));

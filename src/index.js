d63ffe40675bfaa28ef6ea9b367ccebb2402c597
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-03 21:23:43 -0500

    displays the buttons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
-const App = () => <div className="text-center"> Hello you are in React!</div>;
+const keys = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const App = () => (
+  <div id="display" className="display">
+    {keys.map((key, idx) => (
+      <Box text={key} key={idx} />
+    ))}
+  </div>
+);
+const Box = (props) => <div className="box">{props.text}</div>;
+
+ReactDOM.render(<App keys={keys} />, document.querySelector("#root"));
